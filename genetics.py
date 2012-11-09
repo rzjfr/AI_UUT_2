ff59899d8fc824192ab0c8e09dd0553268f7e72f
rzjfr <rzjfr@yahoo.com>
2012-11-09 22:16:51 +0330

    sum function updated and genetic individuals has been made

--- genetics.py
+++ genetics.py
@@ -1,4 +1,4 @@
-
+from random import randint
 cofnt=[]
 powr=[]
 ab=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','w']
@@ -16,7 +16,8 @@ def load_data(file,cofnt=[],powr=[]):
     powr+= f.readline().split()
     f.close()
     return zip(cofnt,powr)
-print load_data("input.txt",cofnt,powr)
+load_data("input.txt",cofnt,powr)
+
 def make_eq(powr,cofnt,i=0,result=""):
     '''
     (list,list)->str
@@ -31,9 +32,14 @@ def make_eq(powr,cofnt,i=0,result=""):
             i+=1
         break
     return result+"0= 0"
+
+
 def len_eq(file,result="",i=0):
     '''
     (filestr)->int
+    dsc: to findout length of equation
+    >>len_eq("input.txt")
+    5
     '''
     f = open(file)
     y= f.readline().split()
@@ -42,19 +48,50 @@ def len_eq(file,result="",i=0):
     f.close()
     return i                        #len(result)/2
     
-print len_eq("input.txt")
 
-def summs(powr,cofnt,result=0):
+
+def individual(length, low, high):
+    '''
+    (int,int,int)->list
+    dsc: Create a random list as a member of the chromosome.
+    example:
+    >>>individual(5,-100,100)
+    [-90, 87, -80, 52, 71]
+    '''
+    return [randint(low,high) for i in xrange(length)]
+indiv=individual(5,-100,100)
+import numpy
+def summs(powr,cofnt,indiv):
     '''
     (list,list,list)->int
+    dsc: summation of equation with real number
+    exmple:
+    
     '''
-    for x in powr:
-        for y in cofnt:
-            #result+= int(y)*(int(z)^int(x))
-            result+= int(y)+int(x)
-            #print x,y
-        break
-    return result
+    
+    pr = numpy.array(powr)
+    cr = numpy.array(cofnt)
+    ir = numpy.array(indiv)
+    print cr*(ir**pr)
+    return sum(cr*(ir**pr))
 
-print summs(powr,cofnt)
- 
+print summs([1,1,1,1,1],[-3,1,1,1,1],[1,-1,1,1,1])
+
+from operator import add   
+def chromosome(count, length, low, high):
+    '''
+    (int,int,int,int)->list
+    dsc: Create a number of random individuals as a chromosome
+    example:
+    >>>chromosome(3,5,0,100)
+    [[94, 99, 59, 77, 48], [100, 100, 87, 80, 38], [5, 100, 92, 18, 75]]
+    '''
+    return [individual(length, low, high) for i in xrange(count)]
+def fitness(individual, target):
+    '''
+    (list,int)->int
+    dsc: returns fitnes of each individual (lower is better)
+    '''
+    sum = reduce(add, individual, 0)
+    return abs(target-sum)
+
